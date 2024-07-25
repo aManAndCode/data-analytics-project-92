6d9e5d56af2e9b565df0fa059a879e07b0c24e41
aManAndCode <105123221+aManAndCode@users.noreply.github.com>
2024-07-26 01:44:02 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -35,8 +35,8 @@ select
     floor(sum(sales.quantity * products.price) / count(sales.sales_id)) as average_income
 from
     sales
-    inner join employees on sales.sales_person_id = employees.employee_id
-    inner join products on sales.product_id = products.product_id
+inner join employees on sales.sales_person_id = employees.employee_id
+inner join products on sales.product_id = products.product_id
 group by
     employees.employee_id,
     employees.first_name,
@@ -50,8 +50,8 @@ having
                 sum(sales.quantity * products.price) / count(sales.sales_id) as total_income
             from
                 sales
-                inner join employees on sales.sales_person_id = employees.employee_id
-                inner join products on sales.product_id = products.product_id
+            inner join employees on sales.sales_person_id = employees.employee_id
+            inner join products on sales.product_id = products.product_id
             group by
                 employees.employee_id
         ) as subquery
@@ -70,8 +70,8 @@ select
     floor(sum(products.price * sales.quantity)) as income
 from
     sales
-    inner join employees on sales.sales_person_id = employees.employee_id
-    inner join products on sales.product_id = products.product_id
+inner join employees on sales.sales_person_id = employees.employee_id
+inner join products on sales.product_id = products.product_id
 group by
     employees.first_name,
     employees.last_name,
@@ -110,7 +110,7 @@ select
     floor(sum(products.price * sales.quantity)) as income
 from
     sales
-    inner join products on sales.product_id = products.product_id
+inner join products on sales.product_id = products.product_id
 group by
     selling_month
 order by
@@ -128,10 +128,11 @@ with the_sales as (
         row_number() over (partition by sales.customer_id order by sales.sale_date, sales.sales_id) as rank
     from
         sales
-        inner join products on sales.product_id = products.product_id
+    inner join products on sales.product_id = products.product_id
     where
         products.price = 0
 ),
+
 first_sales as (
     select
         customer_id,
@@ -143,13 +144,14 @@ first_sales as (
     where
         rank = 1
 )
+
 select
     concat(customers.first_name, ' ', customers.last_name) as customer,
     first_sales.sale_date,
     concat(employees.first_name, ' ', employees.last_name) as seller
 from
     first_sales
-    inner join customers on first_sales.customer_id = customers.customer_id
-    inner join employees on first_sales.sales_person_id = employees.employee_id
+inner join customers on first_sales.customer_id = customers.customer_id
+inner join employees on first_sales.sales_person_id = employees.employee_id
 order by
     customers.customer_id;
